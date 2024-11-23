60969f7bb946fcbf75df3846196e5115f87f03cb
balhyo-younjisang <yjs12180825@gmail.com>
2024-11-23 12:21:15 +0900

    feat: Routing Page
    React-router-dom 의존성을 추가하여 페이지를 라우팅하였습니다. loadable-component 의존성을 추가하여 code spliting 및 pre-load 기능을 추가하였습니다
    styled-components 의존성을 추가하였습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import loadable from "@loadable/component"
+import { Route, Routes } from "react-router-dom"
+
+const MainPage = loadable(() => import("./pages/Main"));
+
 const App = () => {
-  return <></>
+  return <Routes>
+    <Route path="/" element={<MainPage />} loader={MainPage.load} />
+  </Routes>
 }
 
 export default App
