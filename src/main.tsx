72890e8a48e0a10d7a15664992144cf9be637899
balhyo-younjisang <yjs12180825@gmail.com>
2024-11-23 12:30:32 +0900

    styles: add style provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+const windowSize = {
+    small: "screen and (max-width: '600px')",
+    base: "screen and (max-width: '768px')",
+    large: "screen and (max-width: '1024px')",
+};
+
+
+const fontSize = {
+    xs: '0.5rem',
+    sm: "0.75rem",
+    base: '1rem',
+    md: '1.25rem',
+    lg: '1.5rem'
+}
+
+const color = {
+    primary: '#FF5580',
+    secondary: '#FFFFFF'
+}
+
+export const theme = {
+    windowSize,
+    fontSize,
+    color
+}
